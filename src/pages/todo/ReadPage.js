00b22a0ce8e400d1703e783ed610ff911748e1c9
Garbi93 <iseung2093@gmail.com>
2024-04-08 23:25:31 +0900

    04/08 페이지 이동시 쿼리스트링 정보를 유지 하면서 이동 시키기 밋 수정 페이지 추가

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  createSearchParams,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
 function ReadPage(props) {
+  const navigate = useNavigate();
   const { tno } = useParams();
+
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1;
+  const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10;
+
+  const queryStr = createSearchParams({ page: page, size: size }).toString();
+  // createSearchParams 를 사용 하면 ? 뒤의 쿼리 스트링을 만들어준다.
 
   console.log("tno: " + tno);
 
-  return <div className={"text-3xl"}>Todo Read Page</div>;
+  const moveToModify = (tno) => {
+    navigate({ pathname: `/todo/modify/${tno}`, search: queryStr });
+  };
+
+  const moveToList = () => {
+    navigate({ pathname: `/todo/list/`, search: queryStr });
+  };
+
+  return (
+    <div className={"text-3xl"}>
+      Todo Read Page {tno}
+      <div>
+        <button onClick={() => moveToModify(tno)}>Test Modify</button>
+        <button onClick={moveToList}>Test List</button>
+      </div>
+    </div>
+  );
 }
 
 export default ReadPage;
