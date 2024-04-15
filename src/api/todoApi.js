da3d52d95cbe796785163737d75a7b97af7f537d
Garbi93 <iseung2093@gmail.com>
2024-04-15 18:15:11 +0900

    04/15 axios 설치 및 기능별 호출 주소 연결

---
+++ src/api/todoApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// 라우팅 주소
+export const API_SERVER_HOST = "http://localhost:8080";
+
+// 투두 리스트의 고정 api 요청 경로
+const prefix = `${API_SERVER_HOST}/api/todo`;
+
+// 투두 한 개 조회
+export const getOne = async (tno) => {
+  const res = await axios.get(`${prefix}/${tno}`);
+
+  return res.data;
+};
+
+// 투두 목록 조회
+export const getList = async (pageParam) => {
+  const { page, size } = pageParam;
+
+  const res = await axios.get(`${prefix}/list`,{params:{page, size}});
+
+  return res.data;
+};
